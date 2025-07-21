fe31d1339c2c40daf4d34a72c44834098a875740
artem <artemoid@ukr.net>
2025-07-21 06:50:17 +0300

    Improved face cropping and gaze estimation. Introduced alternative labeling that excludes incorrect gaze direction from being marked as artifacts. Trained the Inception v3 model on the newly labeled dataset with cropped face inputs.

--- core/dataset.py
+++ core/dataset.py
@@ -1,33 +1,50 @@
-from torch.utils.data import Dataset
-from PIL import Image
 import os
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
 
 
 class CustomImageDataset(Dataset):
-    def __init__(self, image_dir, transform=None, flip_labels=False):
+    def __init__(self, image_dir, transform=None, labels_csv=None, flip_labels=False):
         self.image_dir = image_dir
-        self.filenames = sorted(os.listdir(image_dir))
         self.transform = transform
         self.flip_labels = flip_labels
+        self.use_csv = labels_csv is not None
+
+        if self.use_csv:
+            self.df = pd.read_csv(labels_csv)
+            self.df['label'] = self.df['label'].astype(int)
+        else:
+            self.filenames = sorted(os.listdir(image_dir))
+        # print(self.__len__())
+
 
     def __len__(self):
-        return len(self.filenames)
+        return len(self.df) if self.use_csv else len(self.filenames)
 
     def __getitem__(self, idx):
-        fname = self.filenames[idx]
-
-        try:
-            label = int(fname.split('_')[-1].split('.')[0])
-        except ValueError:
-            raise ValueError(f"Failed to parse label from filename: {fname}")
+        if self.use_csv:
+            row = self.df.iloc[idx]
+            fname = row['image']
+            label = int(row['label'])
+        else:
+            fname = self.filenames[idx]
+            try:
+                label = int(fname.split('_')[-1].split('.')[0])
+            except ValueError:
+                raise ValueError(f"Failed to parse label from filename: {fname}")
 
         if self.flip_labels:
             label = 1 - label
 
         img_path = os.path.join(self.image_dir, fname)
+        if not os.path.exists(img_path):
+            raise FileNotFoundError(f"Image file {img_path} not found.")
         image = Image.open(img_path).convert("RGB")
 
         if self.transform:
             image = self.transform(image)
+
+        # print(f"_\t{label} ({type(label)})\t{fname} ({type(fname)})")
 
         return image, label, fname
